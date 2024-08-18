f956849a7c7c1c2e87c67fb19efde71d3e63f6eb
Tamim-Shakil <tamim720sk@gmail.com>
2024-08-18 23:14:54 +0600

    Project Done

---
+++ app.js
@@ -0,0 +1,57 @@
+// console.log("test");
+
+// localStorage.setItem("text", "this is a link 2");
+// localStorage.setItem("text1", "this is a link 3");
+// localStorage.setItem("text2", "this is a link 4");
+// localStorage.setItem("text3", "this is a link 5");
+
+// localStorage.clear();
+
+let allLinks = [];
+const input = document.getElementById("input");
+const tabBtn = document.getElementById("tab-btn");
+const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const list = document.getElementById("list");
+
+let getLinksFromLocalStorage = JSON.parse(localStorage.getItem("links"));
+
+if (getLinksFromLocalStorage) {
+  allLinks = getLinksFromLocalStorage;
+  renderArr(allLinks);
+}
+
+function renderArr(arr) {
+  list.innerHTML = "";
+  arr.forEach((item) => {
+    list.innerHTML += `
+  <li><a href=${item} class="link" target="_blank">${item}</a></li>
+  `;
+  });
+}
+
+inputBtn.addEventListener("click", () => {
+  let links = input.value;
+  allLinks.push(links);
+  console.log(allLinks);
+  input.value = "";
+  localStorage.setItem("links", JSON.stringify(allLinks));
+  renderArr(allLinks);
+});
+
+deleteBtn.addEventListener("click", () => {
+  localStorage.clear();
+  allLinks = [];
+  renderArr(allLinks);
+});
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    var activeTab = tabs[0].url;
+    allLinks.push(activeTab);
+    localStorage.setItem("links", JSON.stringify(allLinks));
+    renderArr(allLinks);
+  });
+});
